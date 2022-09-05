3f570348833ff89c6bcc57fa4e55afccf5e59d71
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-05 17:24:10 +0200

    empty

--- src/capture/Main.scala
+++ src/capture/Main.scala
@@ -2,7 +2,7 @@ package capture
 
 @main def Main(names: String*) = {
   def primes = sieve(Stream.from(2))
-  def sieve(s: Stream[Int]): Stream[Int] = Stream.cons(s.head, sieve(s.tail filter { _ % s.head != 0 }))
+  def sieve(s: Stream[Int]): Stream[Int] = s.head #:: sieve(s.tail filter { _ % s.head != 0 })
   primes.take(4000).force
   println("hello world")
 }
