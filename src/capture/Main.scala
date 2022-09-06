9d7ab427215e4180d861a787666e8cd36a68ebdf
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-06 08:38:30 +0200

    empty

--- src/capture/Main.scala
+++ src/capture/Main.scala
@@ -1,8 +1,8 @@
 package capture
 
 @main def Main(names: String*) = {
-  def primes = sieve(Stream.from(2))
-  def sieve(s: Stream[Int]): Stream[Int] = s.head #:: sieve(s.tail filter { _ % s.head != 0 })
+  def primes = sieve(LazyList.from(2))
+  def sieve(s: LazyList[Int]): LazyList[Int] = s.head #:: sieve(s.tail filter { _ % s.head != 0 })
   primes.take(4000).force
   println("hello world")
 }
