6ef514197ca435613f99b6992a9fea33a72fd139
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-08 11:04:19 +0200

    empty

--- src/capture/Main.scala
+++ src/capture/Main.scala
@@ -1,14 +1,14 @@
 package capture
 
+import scala.collection.immutable.Stream
 import Stream.{Cons => cons, Empty, #::}
-import scala.concurrent.ExecutionContext.Implicits.global
 
 @main def Main(names: String*) = {
   val n = 4000
   def primes = sieve(Stream.range(2, n, 1))
   def sieve(s: Stream[Int]): Stream[Int] = s match {
-    case head#::tail => cons(head, tail.map(s => sieve(s filter { _ % head != 0 })))
-    case Empty => Empty
+    case head#::tail => cons(head, sieve(tail filter { _ % head != 0 }))
+    case _ => Empty
   }
   primes.force
   println("hello world")
