7f75b6e7bbbcebffa73159fd1ed0556777fcb417
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-23 17:52:07 +0200

    empty

--- src/capture/Lazy.scala
+++ src/capture/Lazy.scala
@@ -1,9 +1,17 @@
 package capture
 
+import scala.compiletime.uninitialized
+
 trait Lazy[+T] extends (() => T):
   def map[S](f: T -> S) = Lazy(f(apply))
   def flatMap[S](f: T -> Lazy[S]) = f(apply)
 
 object Lazy:
   def apply[T](body: => T): {body} Lazy[T] = new Lazy[T]:
-    lazy val apply = body
+    private var g = () => body
+    private var h: T = uninitialized
+    def apply =
+      if (g ne null)
+        h = g()
+        g = null
+      h
