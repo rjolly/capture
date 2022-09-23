917248e9524a3578fa7aab1487f9fcb84a8512ed
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-23 13:34:55 +0200

    empty

---
+++ src/capture/Lazy.scala
@@ -0,0 +1,9 @@
+package capture
+
+trait Lazy[+T] extends (() => T):
+  def map[S](f: T -> S) = Lazy(f(apply))
+  def flatMap[S](f: T -> Lazy[S]) = f(apply)
+
+object Lazy:
+  def apply[T](body: => T): {body} Lazy[T] = new Lazy[T]:
+    lazy val apply = body
