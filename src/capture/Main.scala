5ace147cb9568483eb0d4c41d19331b78f1bb57c
Raphael Jolly <rjolly@users.sourceforge.net>
2022-09-30 22:21:00 +0200

    empty

--- src/capture/Main.scala
+++ src/capture/Main.scala
@@ -3,7 +3,7 @@ package capture
 import LzyList.{#:, range}
 
 @main def Main(names: String*) = {
-  val m = 37831 // 37831, 104743, 163847, 479939
+  val m = 37831 // 37831, 81817, 128201, 176087, 224743, 274579, 324953, 376133, 427993, 479939
   def primes = sieve(range(2, m + 1))
   def sieve(s: {*} LzyList[Int]): {s} LzyList[Int] = {
     val n = s.head
