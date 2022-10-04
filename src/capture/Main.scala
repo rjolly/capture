e616d4a1f2abf92c1033ccad69e5c83dc2e8e21f
Raphael Jolly <rjolly@users.sourceforge.net>
2022-10-04 13:34:15 +0200

    empty

--- src/capture/Main.scala
+++ src/capture/Main.scala
@@ -3,14 +3,15 @@ package capture
 import LzyList.{#:, range}
 
 @main def Main(names: String*) = {
-  val m = 37831 // 37831, 81817, 128201, 176087, 224743, 274579, 324953, 376133, 427993, 479939
-  def primes = sieve(range(2, m + 1))
+  val m = Array(37831, 81817, 128201, 176087, 224743, 274579, 324953, 376133, 427993, 479939)
+  def primes(max: Int) = sieve(range(2, max + 1))
   def sieve(s: {*} LzyList[Int]): {s} LzyList[Int] = {
     val n = s.head
     n #: s.tail.map(s => sieve(s filter { _ % n != 0 }))
   }
-  var t = System.currentTimeMillis();
-  val r = primes.drop(4000).head
-  t = System.currentTimeMillis() - t;
-  println(s"r: ${r}, t: ${t}")
+  for (i <- 1 to 10)
+    var t = System.currentTimeMillis();
+    val r = primes(m(i - 1)).drop(i * 4000).head
+    t = System.currentTimeMillis() - t;
+    println(s"r: ${r}, t: ${t}")
 }
