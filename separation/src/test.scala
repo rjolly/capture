b055521d5642750133635e2adbda5c4b5e0026ea
Raphael Jolly <rjolly@users.sourceforge.net>
2025-09-19 16:41:02 +0200

    empty

--- separation/src/test.scala
+++ separation/src/test.scala
@@ -1,4 +1,5 @@
 import language.experimental.{captureChecking, separationChecking}
+import scala.caps.Mutable
 
 type Matrix = Array[Int]
 
@@ -9,3 +10,8 @@ def multiply(a: Matrix, b: Matrix, consume c: Matrix^): Unit =
   setElem(c, 0, 0, 0)
 
 @main def Main(names: String*) = ()
+
+class Ref(init: Int) extends Mutable:
+  private var current = init
+  def get: Int = current
+  update def set(x: Int): Unit = current = x
